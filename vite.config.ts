1cdb7cb54e9cde49d4617cabf0e379a25fc75139
mateooosh <mateusz20111@op.pl>
2023-11-14 21:14:19 +0100

    added navbar

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Matchmetric',
+        short_name: 'Matchmetric',
+        description: 'Track your performance.'
+      }
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/common/styles/variables.scss";`,
+      },
+    },
+  },
 })
