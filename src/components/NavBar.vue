f8f5bb88d73bf7ca40cc1e6ae2b6e55e46b4a4b1
mateooosh <mateusz20111@op.pl>
2023-11-14 21:14:27 +0100

    added navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar">
+    <StadiumIcon/>
+    <div>Matchmetric</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import StadiumIcon from '../common/icons/StadiumIcon.vue'
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: $m;
+  width: 100%;
+  background-color: $bc;
+  color: $white;
+  font-size: $xxl;
+  font-weight: 500;
+  padding: $xl 0;
+}
+</style>
